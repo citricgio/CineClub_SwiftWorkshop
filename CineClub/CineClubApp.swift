66d24e9e35554ab484353672107207ba640b1086
Giovanna Micher Santana <111765178+citricgio@users.noreply.github.com>
2024-10-02 21:30:26 -0300

    Initial Commit

---
+++ CineClub/CineClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineClubApp.swift
+//  CineClub
+//
+//  Created by Giovanna Micher on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CineClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineClub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineClub
+//
+//  Created by Giovanna Micher on 02/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
