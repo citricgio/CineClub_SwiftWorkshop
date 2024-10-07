2dc72fa90d7a8961bf0fc7348cff0905809294b9
Giovanna Micher Santana <111765178+citricgio@users.noreply.github.com>
2024-10-07 00:40:41 -0300

    Adição de arrays com informações

---
+++ CineClub/FavoriteMoviesView.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteMoviesView.swift
+//  CineClub
+//
+//  Created by Giovanna Micher on 07/10/24.
+//
+
+import SwiftUI
+
+struct FavoriteMoviesView: View {
+    let moviesNames: [String] = ["Avatar", "Vingadores Ultimato", "Titanic"]
+    let images: [String] = ["avatar-1", "vingadores-ultimato", "titanic"]
+    let releaseDates: [String] = ["10/04/2003", "02/05/2009", "20/02/2015"]
+    let ratings: [String] = ["4.2", "5.0", "4.5"]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteMoviesView()
+}
